b7e7c97edaa7d1b7b84eb4a98736ab180b227bcb
User <michelle-lindsey@hotmail.com>
2025-03-03 14:46:37 -0400

    update site title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: "Shell's Script",
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
